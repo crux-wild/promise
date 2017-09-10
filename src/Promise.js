32af844511c85771506c85e5eba03a8edace9a91
crux wild <18210841568@163.com>
2017-09-10 11:57:13 +0800

    add

--- src/Promise.js
+++ src/Promise.js
@@ -1,3 +1,9 @@
+/**
+ * `Promise`状态机状态枚举类
+ * @private
+ * @readonly
+ * @enum {number}
+ */
 const Status = {
   PENDING: 0,
   FULFILLED: 1,
@@ -6,25 +12,29 @@ const Status = {
 
 /**
  * @private
- * @function
+ * @param {Any} result
  */
 function fulfill(result) {
   this.state = Status.FULFILLED;
   this.value = result;
+  this.handlers.forEach(handle);
+  this.handleers = null;
 }
 
 /**
  * @private
- * @function
+ * @param {Error} error
  */
 function reject(error) {
   this.state = Status.REJECT;
   this.value = error;
+  this.handlers.forEach(handle);
+  this.handleers = null;
 }
 
 /**
  * @private
- * @function
+ * @param {Any|Promise} result
  */
 function resolve(result) {
   try {
@@ -32,7 +42,6 @@ function resolve(result) {
     if (then) {
       doResolve(then.bind(result), resolve, reject);
     }
-
     fulfill(result);
   } catch (err) {
     reject(err);
@@ -41,23 +50,139 @@ function resolve(result) {
 
 /**
  * @private
- * @function
+ * @param {Promise|Any} value
+ * @return {Function|Null}
  */
 function getThen(value) {
+  const t = typeof value;
+  if (value && (t == 'object' || t == 'function')) {
+    const then = value.then;
+    if (typeof then === 'function') {
+      return then;
+    }
+  }
+  return null;
+}
+
+/**
+ * @param {Function} fn
+ * @param {Function} onFulfilled
+ * @param {Function} onRejected
+ */
+function doResolve(fn, onFulfilled, onRejected) {
+  if (this.done === false) return;
+  try {
+    fn((value) => {
+      if (done) return;
+      this.done = true;
+      onFulfilled(value);
+    }, (reason) => {
+      if (done) return;
+      this.done = true;
+      onRejected(reason);
+    });
+  } catch (err) {
+    if (done) return;
+    this.done = true;
+    onRejected(err);
+  }
+}
+
+function handle(handler) {
+  if (this.state === State.PENDING) {
+    handlers.push(handler);
+  } else {
+    if (this.state === State.FULFILLED &&
+      typeof handler.onFulfilled === 'function') {
+
+      handler.onFulfilled(value);
+    }
+    if (this.state === State.REJECTED &&
+      typeof handler.onReject === 'function') {
+
+      handler.onFulfilled(value);
+    }
+  }
 }
 
 /**
  * `Promise`的`es6`实现版本
  *
  * @see https://www.promisejs.org/
+ * @see http://www.ecma-international.org/ecma-262/6.0/#sec-promise-objects/
+ *
  * @public
  * @class
  */
 class Promise {
+  /**
+   * @constructor
+   */
   constructor(fn) {
     this.state = Status.PENDING;
+    this.done = false;
     this.value = null;
     this.handlers = [];
+    this.length = 0;
+
+    doResolve(fn, resolve, reject);
+  }
+
+  static resolve(value) {
+    resolve(value);
+  }
+
+  static all() {
+    // @TODO
+  }
+
+  static race() {
+    // @TODO
+  }
+
+  static reject(reason) {
+    // @TODO
+  }
+
+  done(onFulfilled, onRejected) {
+    setTimeout(() => {
+      handle({
+        onFulfilled,
+        onRejected,
+      });
+    }, 0);
+  }
+
+  then(onFulfilled, onRejected) {
+    return new Promise((resolve, reject) => {
+      return this.done(
+        (result) => {
+          if (typeof onFulfilled === 'function') {
+            try {
+              return resolve(onFulFilled(result));
+            } catch (err) {
+              return reject(err);
+            }
+          } else {
+            return resolve(result);
+          }
+        });
+      },
+      (error) => {
+        if (typeof onRejected === 'function') {
+          try {
+            return resolve(onRejected(error));
+          } catch (err) {
+            return reject(err);
+          }
+        } else {
+          return reject(err);
+        }
+      }
+    );
+  }
+
+  catch(onRejected) {
   }
 }
 
