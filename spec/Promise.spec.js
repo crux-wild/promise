8b669578818227a975a2dbc3a00ef792aca500f4
crux wild <18210841568@163.com>
2017-09-10 23:14:10 +0800

    add

--- spec/Promise.spec.js
+++ spec/Promise.spec.js
@@ -1,1 +1,11 @@
 import test from 'ava';
+
+import Promise from 'Promise';
+
+test((test) => {
+  const promise = new Promise((resolve, reject) => {
+    resolve(1);
+  });
+
+  test.deepEqual(promise.length, 1);
+});
